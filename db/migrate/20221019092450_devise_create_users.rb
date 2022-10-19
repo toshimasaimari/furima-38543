366478ecd5bcc46653cf05b2ddbd61c5df59103c
toshimasaimari <toshimasa.imari@gmail.com>
2022-10-19 22:23:44 +0900

    USERモデルのバリデーション設定の修正

--- db/migrate/20221019092450_devise_create_users.rb
+++ db/migrate/20221019092450_devise_create_users.rb
@@ -9,7 +9,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :last_name_kana,     null: false
       t.string :first_name_kana,    null: false
       t.date   :birthday,           null: false
-      t.string :email,              null: false, unique: true, default: ""
+      t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
