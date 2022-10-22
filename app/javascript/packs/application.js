e615e4a66c307b2e2f6a69f0a3e9c4e61be79544
toshimasaimari <toshimasa.imari@gmail.com>
2022-10-22 11:45:22 +0900

    小数点以下切り捨て処理へ修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,8 +19,8 @@ window.addEventListener('load', function(){
 const productCost = document.getElementById("item-price")
 productCost.addEventListener('input', function(){
   const productTaxCost = document.getElementById("add-tax-price")
-  productTaxCost.innerHTML = Math.round(productCost.value * 0.1 )
+  productTaxCost.innerHTML = Math.floor(productCost.value * 0.1 )
   const productTaxFree = document.getElementById("profit")
-  productTaxFree.innerHTML = Math.round(productCost.value - Math.round(productCost.value * 0.1 ))
+  productTaxFree.innerHTML = Math.floor(productCost.value - Math.floor(productCost.value * 0.1 ))
 })
 })
