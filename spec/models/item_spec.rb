19cf8011a1afa8363ad10770cfdc28e375930588
toshimasaimari <toshimasa.imari@gmail.com>
2022-10-22 20:52:00 +0900

    テストコード、ユーザーが紐付いていなければ投稿できない追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -76,6 +76,11 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include "Shipping day can't be blank"
       end
+      it "ユーザーが紐付いていなければ投稿できない" do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include "User must exist"
+      end
     end
   end
 end
