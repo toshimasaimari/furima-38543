070e01b0016ce5f46663069a47c6e9b3829949cb
toshimasaimari <toshimasa.imari@gmail.com>
2022-10-22 11:15:06 +0900

    商品出品機能に関するテストコード実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    product_name          {Faker::Name.initials(number: 2)}
+    product_description   {Faker::Name.initials(number: 2)}
+    category_id           {Faker::Number.between(from: 2, to: 11)}
+    condition_id          {Faker::Number.between(from: 2, to: 7)}
+    shipping_fee_id       {Faker::Number.between(from: 2, to: 3)}
+    prefecture_id         {Faker::Number.between(from: 2, to: 48)}
+    shipping_day_id       {Faker::Number.between(from: 2, to: 4)}
+    price                 {Faker::Number.between(from: 300, to: 9999999)}
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('app/assets/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
